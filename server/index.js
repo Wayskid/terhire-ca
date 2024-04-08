9df6c612fcb2bc2769a5749fe11db9dd5cb07627
Wayskid <wayskid02@gmail.com>
2024-04-08 04:34:19 +0100

    first commit

--- server/index.js
+++ server/index.js
@@ -40,7 +40,6 @@ app.use(
       "http://localhost:5173",
       "https://terhire.com",
       "https://terhire-ca.netlify.app",
-      "https://terhire-ca.onrender.com/api/stripe/create-checkout-session",
     ],
     methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
     optionSuccessStatus: 204,
@@ -51,8 +50,12 @@ app.use(
 const server = http.createServer(app);
 export const io = new Server(server, {
   cors: {
-    origin,
-    methods,
+    origin: [
+      "http://localhost:5173",
+      "https://terhire.com",
+      "https://terhire-ca.netlify.app",
+    ],
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
   },
 });
 
