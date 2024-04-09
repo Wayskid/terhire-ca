b0b53a0a2243787fe553b3643413b7d9a85c7d13
Wayskid <wayskid02@gmail.com>
2024-04-09 04:04:15 +0100

    finishing touches

--- client/src/layout/AnnouncementBar.jsx
+++ client/src/layout/AnnouncementBar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 export default function AnnouncementBar() {
   return (
     <div className={`bg-main text-white text-center py-3`}>
-      Shop Terhire <Link className="underline">Shop now</Link>
+      Sign up today and get a <b>10%</b> discount!
     </div>
   );
 }
